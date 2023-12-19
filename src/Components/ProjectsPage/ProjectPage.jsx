9491f76a49a1f15846578dc29f58984a9e55430b
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2023-12-19 17:15:43 +0600

    all done

--- src/Components/ProjectsPage/ProjectPage.jsx
+++ src/Components/ProjectsPage/ProjectPage.jsx
@@ -1,11 +1,12 @@
 import ServicesBenner from "../ServicesPage/servicesComponents/ServicesBenner";
-import Tabs from "./ProjectPageComponents/Tabs";
+import TabsKeep from "./ProjectPageComponents/Tabs";
+
 
 const ProjectPage = () => {
     return (
         <div>
              <ServicesBenner img={"https://i.ibb.co/8rq16zQ/istockphoto-1469653478-2048x2048.jpg"}></ServicesBenner>
-            <Tabs></Tabs>
+     <TabsKeep></TabsKeep>
         </div>
     );
 };
--- src/Components/ProjectsPage/ProjectPageComponents/Tabs.jsx
+++ src/Components/ProjectsPage/ProjectPageComponents/Tabs.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
-
-const Tabs = () => {
-    return (
-<p></p>
-    );
+import { Tabs } from "keep-react";
+import { Gear, GridFour, Phone, User, WarningCircle } from "phosphor-react";
+const TabsKeep = () => {
+  return (
+    <Tabs
+    aria-label="Tabs"
+    style="underline"
+    borderPosition="bottom"
+    iconPosition="left"
+    className="text-blue-700"
+  >
+    <Tabs.Item title="All " icon={<User size={20} />}>
+      Profile content
+    </Tabs.Item>
+    <Tabs.Item title="Excavation" icon={<GridFour size={20} />}>
+      Dashboard content
+    </Tabs.Item>
+    <Tabs.Item title="Foundation" icon={<Gear size={20} />}>
+      Settings content
+    </Tabs.Item>
+    <Tabs.Item title="Superstructure" icon={<Phone size={20} />}>
+      Contacts content
+    </Tabs.Item>
+  </Tabs>
+  );
 };
 
-export default Tabs;
+export default TabsKeep;
